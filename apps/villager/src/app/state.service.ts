9787e26655daa032ea82e359029cf0b4113319aa
Jennifer Wadella <jennifer.wadella@gmail.com>
2020-12-01 17:20:05 -0600

    state file

--- apps/villager/src/app/state.service.ts
+++ apps/villager/src/app/state.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@angular/core';
+import produce from "immer";
+
+const baseState = {
+  villagers: [],
+  selectedSortOption: undefined,
+  filterOptions: [],
+  showOnlyFavorites: false
+}
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +15,11 @@ import { Injectable } from '@angular/core';
 export class StateService {
 
   constructor() { }
+
+  update(action: any, payload: any) {
+    return produce(baseState, draftState => {
+      draftState.push({todo: "Tweet about it"})
+      draftState[1].done = true
+  })
+  }
 }
