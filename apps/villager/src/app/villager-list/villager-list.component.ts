ad9853e47c38dbc90a854f0e2ae1b756e749075c
Jennifer Wadella <jennifer.wadella@gmail.com>
2020-12-02 09:29:32 -0600

    removing code

--- apps/villager/src/app/villager-list/villager-list.component.ts
+++ apps/villager/src/app/villager-list/villager-list.component.ts
@@ -1,9 +1,8 @@
 import { getDisplayVillagers } from './../+state/selectors';
 import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Villager, Personality, Species, Hobby, VillagerSortOptions, AcnhService } from '@animal-crossing/api';
+import { Villager, Personality, Species, Hobby, VillagerSortOptions } from '@animal-crossing/api';
 import { MatCheckbox, MatCheckboxChange } from '@angular/material/checkbox';
-import {MatAccordion, MatExpansionPanel} from '@angular/material/expansion';
+import { MatExpansionPanel} from '@angular/material/expansion';
 import { Store } from '@ngrx/store';
 import * as VillagerActions from '../+state/actions';
 import { Observable } from 'rxjs';
@@ -17,26 +16,21 @@ export class VillagerListComponent implements OnInit {
   @ViewChild(MatExpansionPanel) accordion!: MatExpansionPanel;
   @ViewChildren(MatCheckbox) checkboxes!: QueryList<MatCheckbox>;
 
-  private villagers: Villager[] = [];
-  //public displayedVillagers: Villager[] = [];
   public displayedVillagers$: Observable<Villager[]> = this.store.select(getDisplayVillagers);
   private checkSelection: VillagerSortOptions[] = [];
   public sortOptions = ["personality", "species", "hobby", "birthday"];
-  public personalities = Object.values(Personality);
-  public species = Object.values(Species);
-  public hobbies = Object.values(Hobby);
   public checkBoxList = [
     {
       attribute: 'personality',
-      list: this.personalities
+      list: Object.values(Personality)
     },
     {
       attribute: 'species',
-      list: this.species
+      list: Object.values(Species)
     },
     {
       attribute: 'hobby',
-      list: this.hobbies
+      list: Object.values(Hobby)
     }
   ]
 
@@ -46,39 +40,9 @@ export class VillagerListComponent implements OnInit {
     this.store.dispatch(VillagerActions.LoadVillagers());
   }
 
-  sortList(list: Villager[], property: VillagerSortOptions) {
-    if(property === "birthday") {
-      list.sort(function(a,b){
-        var [dayA, monthA] = a.birthday.split('/');
-        var dateA = new Date(2020, parseInt(monthA, 10), parseInt(dayA, 10));
-
-        var [dayB, monthB] = b.birthday.split('/');
-        var dateB = new Date(2020, parseInt(monthB, 10), parseInt(dayB, 10));
-
-        return dateA.getTime() - dateB.getTime()
-      });
-    }
-    else {
-      return list.sort(function(a, b) {
-        var nameA = a[property].toUpperCase();
-        var nameB = b[property].toUpperCase();
-  
-        if (nameA < nameB) {
-          return -1;
-        }
-        if (nameA > nameB) {
-          return 1;
-        }    
-        return 0;
-      });
-    }
-
-  }
-
   filterList() {
     this.accordion.close();
     this.store.dispatch(VillagerActions.SelectFilters({filterOptions: this.checkSelection}));
-
   }
 
   showFavorites(change: MatCheckboxChange) {
